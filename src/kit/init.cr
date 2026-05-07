d85fdd389f27a722f5e19d6281383fb5eb4d09cf
ZenDrx <ynwghosted@icloud.com>
2026-05-07 11:24:09 +0100

    Refactor .anv initialization and key management
    Refactor initialization process for .anv file and master key. Ensure .anv directory is created and added to .gitignore if it doesn't exist.

--- src/kit/init.cr
+++ src/kit/init.cr
@@ -3,29 +3,37 @@ require "base64"
 require "random/secure"
 
 def init
- puts "...initializing .anv..."
-if File.exist?(".anv")
- puts ".anv file exists use -h for help")
-else 
- File.touch(".anv")
- puts "created .anv file starting encryption.."
- start_encryption
+  puts "...initializing .anv..."
+  if File.exist?(".anv")
+    puts ".anv file exists use -h for help"
+  else
+    Dir.mkdir_p(".anv")
+    key = Random::Secure.hex(32)
+    File.write(".anv/master.key", key)
+    File.chmod(".anv/master.key", 0o600)
+    if File.exist?(".gitignore")
+      ignore_content = File.read(".gitignore")
+      unless ignore_content.includes?(".anv/master.key")
+        File.open(".gitignore", "a") do |file|
+          file.puts "\n.anv/master.key"
+        end
+      end
+    else
+      File.write(".gitignore", ".anv/master.key\n")
+    end
+    template_data = "{}"
+    cipher = OpenSSL::Cipher.new("AES-256-GCM")
+    cipher.encrypt
+    cipher.key = key.hexbytes
+    iv = Random::Secure.random_bytes(12)
+    cipher.iv = iv
+    encrypted = cipher.update(template_data) + cipher.final
+    tag = cipher.auth_tag
+    combined = iv + tag + encrypted
+    encoded = Base64.encode(combined)
+    File.write(".anv/store", encoded)
+    puts ".anv initialized successfully"
+    puts "master key saved to .anv/master.key"
+    puts "do NOT commit .anv/master.key to git"
+  end
 end
-
-def start_encryption
-  key = Random::Secure.hex(32)
-  puts "saving key.."
-  Dir.mkdir_p("anv")
-  File.write("anv/master.key", key)
-  template_data = "anv secret protector"
-  cipher = OpenSSL::Cipher.new("AES-256-GCM")
-  cipher.encrypt 
-  cipher.key = key.hexabyte
-  iv = Random::Secure.random_bytes(12)
-  cipher.iv = iv
-  encrypted = cipher.update(template_data) + cipher.final
-  tag = cipher.auth_tag
-  combined = iv + tag + encrypted 
-  Base64.encode(combined)
-end 
-   
