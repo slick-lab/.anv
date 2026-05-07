293b8f38e325984207944ee5dcdb60463513f8cc
ZenDrx <ynwghosted@icloud.com>
2026-05-07 13:10:45 +0100

    Add run command to execute commands with secrets

--- src/anv.cr
+++ src/anv.cr
@@ -2,6 +2,41 @@ require "./kit/init"
 require "./kit/set_get"
 require "./kit/en_de"
 
+def run(command_args : Array(String))
+  if command_args.empty?
+    puts "ERROR: No command specified"
+    puts "Usage: anv run -- <command> [args]"
+    return
+  end
+  
+  unless File.exist?(".anv/store")
+    puts "ERROR: No store found. Run `anv init` first."
+    return
+  end
+  
+  master_key = read_master_key
+  if master_key.nil?
+    puts "ERROR: Master key not found. Run `anv init` first."
+    return
+  end
+  
+  encrypted_blob = File.read(".anv/store")
+  plain = decrypt(encrypted_blob, master_key)
+  if plain.nil?
+    puts "ERROR: Decryption failed"
+    return
+  end
+  
+  data = JSON.parse(plain).as_h
+  
+  env = ENV.to_h
+  data.each do |key, value|
+    env[key.to_s] = value.to_s
+  end
+  
+  Process.run(command_args[0], command_args[1..-1] || [] of String, env: env, shell: true)
+end
+
 def main
   if ARGV.size < 1
     puts "Usage: anv <command> [arguments]"
@@ -10,6 +45,7 @@ def main
     puts "  init                     Initialize anv in current directory"
     puts "  set KEY=value            Set a secret"
     puts "  get KEY                  Get a secret"
+    puts "  run -- <command>         Run command with secrets injected"
     puts "  help                     Show this help message"
     exit 1
   end
@@ -31,6 +67,12 @@ def main
       exit 1
     end
     get(ARGV[1])
+  when "run"
+    args = ARGV[1..-1] || [] of String
+    if args.size > 0 && args[0] == "--"
+      args = args[1..-1] || [] of String
+    end
+    run(args)
   when "help"
     puts "Usage: anv <command> [arguments]"
     puts ""
@@ -38,6 +80,7 @@ def main
     puts "  init                     Initialize anv in current directory"
     puts "  set KEY=value            Set a secret"
     puts "  get KEY                  Get a secret"
+    puts "  run -- <command>         Run command with secrets injected"
     puts "  help                     Show this help message"
   else
     puts "ERROR: Unknown command '#{command}'"
