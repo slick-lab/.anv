0315a3df0ea30ace3f4d75dabea636942ea319d8
ZenDrx <ynwghosted@icloud.com>
2026-05-07 12:18:57 +0100

    Refactor set and get functions for better clarity

--- src/kit/set_get.cr
+++ src/kit/set_get.cr
@@ -1,34 +1,34 @@
-
-
-def set(key_value : String)
+def set(key_value : String) : String?
   key, value = key_value.split("=", 2)
-  if File.exist?(".anv/store")
-   encrypted = File.read(".anv/store")
-   plain = decrypt(encrypted, read_master_key)
-   data = JSON.parse(plain)
- else 
-  puts "no store to set pls run init or -h"
- end
- data[key] = value
- new_plain = data.to_json
- new_encrypt = encrypt(new_plain, read_master_key)
- File.write(".anv/store", new_encrypt)
- puts " successfully set #{key}"
-end 
-
-def get(key : String)
-if File.exist?(".anv/store")
- encrypted = File.read(".anv/store")
- plain = decrypt(encrypted, read_master_key)
- data = JSON.parse(plain)
-else 
- puts ".anv/store does not exist pls run init or -h"
-end 
- if value = data[key]?
-  p value.as_s
- else
-  puts "key not found for #{key}"
-end 
-end 
-
   
+  unless File.exist?(".anv/store")
+    return "no store to set pls run init or -h"
+  end
+  
+  encrypted = File.read(".anv/store")
+  plain = decrypt(encrypted, read_master_key)
+  data = JSON.parse(plain)
+  
+  data[key] = value
+  new_plain = data.to_json
+  new_encrypt = encrypt(new_plain, read_master_key)
+  File.write(".anv/store", new_encrypt)
+  
+  return "successfully set #{key}"
+end 
+
+def get(key : String) : String?
+  unless File.exist?(".anv/store")
+    return ".anv/store does not exist pls run init or -h"
+  end
+  
+  encrypted = File.read(".anv/store")
+  plain = decrypt(encrypted, read_master_key)
+  data = JSON.parse(plain)
+  
+  if value = data[key]?
+    return value.as_s
+  else
+    return "key not found for #{key}"
+  end
+end
